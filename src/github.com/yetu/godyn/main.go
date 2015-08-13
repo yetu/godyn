43c1ec8f487990764707970732aa5c218e18073a
Till Klocke <till.klocke@yetu.com>
2015-08-13 15:49:52 +0200

    Publishing changes now finally works

--- src/github.com/yetu/godyn/main.go
+++ src/github.com/yetu/godyn/main.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -16,9 +17,21 @@ import (
 type Rdata struct {
 	Address string `json:"address"`
 }
-type UpdateZoneRequest struct {
+type ARecord struct {
+	RecordData Rdata `json:"rdata"`
+	Ttl        int   `json:"ttl"`
+}
+type CreateRecordRequest struct {
 	Rdata Rdata `json:"rdata"`
 	Ttl   int   `json:"ttl"`
+}
+
+type UpdateRecordRequest struct {
+	ARecords []ARecord `json:"ARecords"`
+}
+
+type Publish struct {
+	Publish string `json:"publish"`
 }
 
 /*
@@ -39,7 +52,6 @@ func getPublicIpFromHosts() (ip string, err error) {
 			result := hostRegex.FindStringSubmatch(line)
 			ip = result[1]
 			err = nil
-			log.Printf("Hooray, we found a valid first line in the hosts file with IP %s", result[1])
 			return result[1], nil
 		}
 	}
@@ -48,6 +60,7 @@ func getPublicIpFromHosts() (ip string, err error) {
 }
 
 func main() {
+	flag.Parse()
 	customer := os.Getenv("DYNECT_CUSTOMER")
 	username := os.Getenv("DYNECT_USERNAME")
 	password := os.Getenv("DYNECT_PASSWORD")
@@ -62,22 +75,58 @@ func main() {
 	if err != nil {
 		log.Panicf("Can't determine public IP for this container: %v", err)
 	}
-	zoneReq := &UpdateZoneRequest{
+	/*zoneReq := &CreateRecordRequest{
 		Rdata: Rdata{
 			Address: publicIp,
 		},
 		Ttl: 0,
+	}*/
+
+	updateReq := &UpdateRecordRequest{
+		[]ARecord{
+			{Rdata{publicIp}, 0},
+		},
 	}
-	zreqBytes, err := json.Marshal(zoneReq)
+	zreqBytes, err := json.Marshal(updateReq)
+	//log.Printf("Request Payload: %s", string(zreqBytes))
 	if err != nil {
 		log.Panicf("Can't marshal zone request: %v", err)
 	}
 
 	path := fmt.Sprintf("ARecord/%s/%s/", zone, fqdn)
-	_, err = client.Request("POST", path, bytes.NewReader(zreqBytes))
+	responseBytes, err := client.Request("PUT", path, bytes.NewReader(zreqBytes))
 	if err != nil {
 		log.Panicf("Failed to update FQDN %s for this host: %v", fqdn, err)
 	} else {
-		log.Printf("Updated FQDN %s with A record for IP %s", fqdn, publicIp)
+		responseString := string(responseBytes)
+		response := &dynect.Response{}
+		if err := json.Unmarshal(responseBytes, response); err != nil {
+			log.Panicf("Can't unmarshall response from dynect: %s Error %v", responseString, err)
+		}
+		if response.Status == "success" {
+			log.Printf("Successfully updated FQDN %s to IP %s", fqdn, publicIp)
+			publishRequest := &Publish{Publish: "True"}
+			publishRequestBytes, err := json.Marshal(publishRequest)
+			if err != nil {
+				log.Panicf("Can't marshal publish zone request: %v", err)
+			}
+			publishPath := fmt.Sprintf("Zone/%s", zone)
+			prResultBytes, err := client.Request("PUT", publishPath, bytes.NewReader(publishRequestBytes))
+			if err != nil {
+				log.Panicf("Can't publish changes: %v", err)
+			} else {
+				prResult := &dynect.Response{}
+				if err := json.Unmarshal(prResultBytes, prResult); err != nil {
+					log.Panicf("Can't unmarshall response from dynect. Error %v Response %s", err, string(prResultBytes))
+				}
+				if prResult.Status == "success" {
+					log.Printf("Successfully published changes to Dynect")
+				} else {
+					log.Panicf("Failed to publish changes to Dynect: %s", string(prResultBytes))
+				}
+			}
+		} else {
+			log.Panicf("Failed to modify zone in Dynect. Response: %s", responseString)
+		}
 	}
 }
